6d6fbe73af54548a093df11a264a5bcc2166765c
2411github2021 <quen_tar@mail.ru>
2021-12-17 18:41:49 +0200

    margin_form

--- src/Login.js
+++ src/Login.js
@@ -7,14 +7,10 @@ function Login() {
                 <span>E-mail </span>
                 <input className="mail" placeholder='Your e-mail' />
             </div>
-            <br />
             <div className="form_pass">
                 <span>Password </span>
                 <input className="pass" placeholder='Your password' />
             </div>
-            <br />
-            <br />
-            <br />
             <div>
                 <input className="button" type={"submit"} ></input>
             </div>
