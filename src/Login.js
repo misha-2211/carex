d0e7361f74facde7624458ea9d50b219bfd5dece
2411github2021 <quen_tar@mail.ru>
2022-01-18 11:06:45 +0200

    Auth

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,30 @@
 import './Login.css';
 
-function Login() {
+function Login(props) {
+
+    const interLogin = (e) => {
+        e.preventDefault()
+        props.history.push('/')
+    }
+
     return (
-        <form className='form'>
+        <form className='form' onSubmit={interLogin}>
             <div className='form-mail'>
                 <span>E-mail </span>
-                <input className="mail" placeholder='Your e-mail' />
+                <input
+                    className="mail" placeholder='Your e-mail' required
+                />
             </div>
+
             <div className="form-pass">
                 <span>Password </span>
-                <input className="pass" placeholder='Your password' />
+                <input
+                    className="pass" placeholder='Your password' type="password" required
+                />
             </div>
+
             <div>
-                <button className="but" type={"submit"} >Отправить</button>
+                <button className="but" type="submit">Отправить</button>
             </div>
         </form >
     );
